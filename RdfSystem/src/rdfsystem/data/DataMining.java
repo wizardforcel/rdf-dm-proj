35d43581aa69afae674f7c94a506ba77ec38bc8d
wizardforcel <562826179@qq.com>
2015-05-23 20:15:51 +0800

    5.23

--- RdfSystem/src/rdfsystem/data/DataMining.java
+++ RdfSystem/src/rdfsystem/data/DataMining.java
@@ -23,6 +23,7 @@ import weka.clusterers.SimpleKMeans;
 import weka.core.*;
 import weka.filters.Filter;
 import weka.filters.unsupervised.attribute.NumericToNominal;
+import weka.filters.unsupervised.attribute.StringToNominal;
 import weka.filters.unsupervised.attribute.StringToWordVector;
 
 /**
@@ -84,12 +85,73 @@ public class DataMining
             ins.add(new Instance(1.0, row));
         }
         
-        NumericToNominal f1 = new NumericToNominal();
-        f1.setInputFormat(ins);
-        ins = Filter.useFilter(ins, f1);
+        if(hasYear)
+        {
+            NumericToNominal f1 = new NumericToNominal();
+            f1.setInputFormat(ins);
+            ins = Filter.useFilter(ins, f1);
+        }
         
         return ins;
     }
+    
+    /*private static Instances transformData(RdfManager manager, boolean hasYear) 
+            throws Exception
+    {
+        FastVector attrs = new FastVector();
+        if(hasYear)
+        {
+            Attribute yearAttr = new Attribute("year");
+            attrs.addElement(yearAttr);
+        }
+        
+        Set<String> authorSet = getAuthorSet(manager);
+        FastVector authors = new FastVector();
+        for(String author : authorSet)
+            authors.addElement(author);
+        Attribute authorAttr = new Attribute("author", authors);
+        attrs.addElement(authorAttr);
+        
+        Set<String> labelSet = getLabelSet(manager);
+        FastVector labels = new FastVector();
+        for(String label : labelSet)
+            labels.addElement(label);
+        Attribute labelAttr = new Attribute("label", labels);
+        attrs.addElement(labelAttr);
+        
+        Instances ins = new Instances("paper", attrs, 0);
+        
+        for(Map.Entry<String, Paper> item : manager)
+        {
+            Paper p = item.getValue();
+            
+            for(Author au : p.getList())
+            {
+                for(String label : p.getLabel())
+                {
+                    double[] row = new double[ins.numAttributes()];
+                    if(hasYear)
+                        row[ins.attribute("year").index()] 
+                                = p.getYear();
+                    row[ins.attribute("author").index()]
+                            = ins.attribute("author")
+                                 .indexOfValue(String.valueOf(au.getId()));
+                    row[ins.attribute("label").index()]
+                            = ins.attribute("label").indexOfValue(label);
+                    ins.add(new Instance(1.0, row));
+                }
+            }
+        }
+        
+        if(hasYear)
+        {
+            NumericToNominal f1 = new NumericToNominal();
+            f1.setInputFormat(ins);
+            ins = Filter.useFilter(ins, f1);
+        }
+
+        return ins;
+    }*/
     
     public static String classify(RdfManager manager) 
            throws Exception
@@ -127,6 +189,30 @@ public class DataMining
             System.out.println(rule);
 
         return null;
+    }
+    
+    private static Set<String> getLabelSet(RdfManager manager)
+    {
+        Set<String> set = new HashSet<>();
+        for(Map.Entry<String, Paper> item : manager)
+        {
+            Paper p = item.getValue();
+            for(String label : p.getLabel())
+                set.add(label);
+        }
+        return set;
+    }
+    
+    private static Set<String> getAuthorSet(RdfManager manager)
+    {
+        Set<String> set = new HashSet<>();
+        for(Map.Entry<String, Paper> item : manager)
+        {
+            Paper p = item.getValue();
+            for(Author au : p.getList())
+                set.add("" + au.getId());
+        }
+        return set;
     }
     
     private static Set<String> getAllWords(RdfManager manager)
